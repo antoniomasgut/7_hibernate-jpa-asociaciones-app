eceafe11d8beb9aaf230d4e7ce4bb9cbe0064ba0
antoniomasgut <antoniomasgut@gmail.com>
2022-12-12 09:18:01 +0100

    Initial commit

---
+++ hibernate-jpa-asociaciones-app/src/main/java/org/daw/hibernateapp/HibernateAsociacionesManyToOneFind.java
@@ -0,0 +1,31 @@
+package org.daw.hibernateapp;
+
+import jakarta.persistence.EntityManager;
+import org.daw.hibernateapp.entity.Cliente;
+import org.daw.hibernateapp.entity.Factura;
+import org.daw.hibernateapp.util.JpaUtil;
+
+public class HibernateAsociacionesManyToOneFind {
+    public static void main(String[] args) {
+
+        EntityManager em = JpaUtil.getEntityManager();
+
+        try {
+
+            em.getTransaction().begin();
+            Cliente cliente = em.find(Cliente.class, 1L);
+
+            Factura factura = new Factura("compras de oficina", 1000L);
+            factura.setCliente(cliente);
+            em.persist(factura);
+
+            System.out.println(factura.getCliente());
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+}
